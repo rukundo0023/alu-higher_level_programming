1a5d44d1677630dc89fa22f7388a7a26c724af9b
rukundo0023 <r.nshimiyim@alustudent.com>
2023-03-07 05:45:22 -0800

    all tasks

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for col in i:
+            print("{:d}".format(col), end=" " if col != i[-1] else "")
+        print()
