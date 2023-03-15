04e45ede58bf3abcb074d16621243d5ca0bb2ee9
rukundo0023 <r.nshimiyim@alustudent.com>
2023-03-15 15:13:26 -0700

    my hard one

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,42 +1,48 @@
 #!/usr/bin/python3
-class Rectangle:
-    """Defines a rectangle
-    """
+"""Rectangle module.
+This module contains a class that defines a rectangle.
+"""
+
+
+class Rectangle():
+    """Defines a rectangle."""
+
     def __init__(self, width=0, height=0):
-        """Instantiation
+        """Sets the necessary attributes for the Rectangle object.
+        Args:
+            width (int): the width of the rectangle.
+            height (int): the height of the rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Getter method
-        """
+        """Get or set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """"Setter method
-        """
-        if type(value) is not int:
+        if type(value) is int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
             raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     @property
     def height(self):
-        """Getter method
-        """
+        """Get or set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter method
-        """
-        if type(value) is not int:
+        if type(value) is int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
 
