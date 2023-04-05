7289dcbf61349e21e2d1da3e84ad3f821e411234
rukundo0023 <r.nshimiyim@alustudent.com>
2023-04-05 19:36:27 +0000

    first tks

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,2 +1,4 @@
-Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+#!/bin/bash
+#  a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -sI "$1" | awk '/Content-Length/ {print $2}'
+
