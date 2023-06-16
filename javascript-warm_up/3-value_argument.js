ac4c9994ca78f504786b0884a55a009d643476c4
rukundo0023 <r.nshimiyim@alustudent.com>
2023-06-15 18:44:46 -0700

    3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const arg1 = process.argv2;
-const arg2 = process.argv3;
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
-console.log(arg1 + 'is' +arg2);
+console.log(arg1 + " is " + arg2);
+
