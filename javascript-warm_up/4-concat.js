728533b871e571a93643d97175629b5a564aa562
rukundo0023 <r.nshimiyim@alustudent.com>
2023-06-15 19:36:58 -0700

    4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-const printArguments = (arg1, arg2) => {
-  console.log(`${arg1} is ${arg2}`);
-};
-
-printArguments('Python', 'fun');
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
